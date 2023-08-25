77875b75957aa8236d924144fc657155a04f084d
faizal108 <69467378+faizal108@users.noreply.github.com>
2023-08-25 20:53:56 +0530

    Initial Setup

---
+++ src/main/java/com/faizal/HybridDatabase/HybridDatabaseApplication.java
@@ -0,0 +1,13 @@
+package com.faizal.HybridDatabase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HybridDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HybridDatabaseApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/faizal/HybridDatabase/HybridDatabaseApplicationTests.java
@@ -0,0 +1,13 @@
+package com.faizal.HybridDatabase;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HybridDatabaseApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
